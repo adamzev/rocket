611e2f936738d26abf1ea86254bb4562ffec94ff
tutordelphia <AdamLevin@tutordelphia.com>
2017-05-19 21:17:02 -0400

    adds first windows dist

--- main.py
+++ main.py
@@ -11,6 +11,72 @@ import util.title as title
 from libs import *
 from libs.vehicleFactory import VehicleFactory
 
+import os
+import ctypes
+import msvcrt
+import subprocess
+
+from ctypes import wintypes
+
+kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+user32 = ctypes.WinDLL('user32', use_last_error=True)
+
+SW_MAXIMIZE = 3
+
+kernel32.GetConsoleWindow.restype = wintypes.HWND
+kernel32.GetLargestConsoleWindowSize.restype = wintypes._COORD
+kernel32.GetLargestConsoleWindowSize.argtypes = (wintypes.HANDLE,)
+user32.ShowWindow.argtypes = (wintypes.HWND, ctypes.c_int)
+
+
+import ctypes
+
+LF_FACESIZE = 32
+STD_OUTPUT_HANDLE = -11
+
+class COORD(ctypes.Structure):
+	_fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+class CONSOLE_FONT_INFOEX(ctypes.Structure):
+	_fields_ = [("cbSize", ctypes.c_ulong),
+				("nFont", ctypes.c_ulong),
+				("dwFontSize", COORD),
+				("FontFamily", ctypes.c_uint),
+				("FontWeight", ctypes.c_uint),
+				("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+font = CONSOLE_FONT_INFOEX()
+font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+font.nFont = 12
+font.dwFontSize.X = 11
+font.dwFontSize.Y = 18
+font.FontFamily = 54
+font.FontWeight = 400
+font.FaceName = "Lucida Console"
+
+handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+		handle, ctypes.c_long(False), ctypes.pointer(font))
+def maximize_console(lines=None):
+	fd = os.open('CONOUT$', os.O_RDWR)
+	try:
+		hCon = msvcrt.get_osfhandle(fd)
+		max_size = kernel32.GetLargestConsoleWindowSize(hCon)
+		if max_size.X == 0 and max_size.Y == 0:
+			raise ctypes.WinError(ctypes.get_last_error())
+	finally:
+		os.close(fd)
+	cols = max_size.X
+	hWnd = kernel32.GetConsoleWindow()
+	if cols and hWnd:
+		if lines is None:
+			lines = max_size.Y
+		else:
+			lines = max(min(lines, 9999), max_size.Y)
+		subprocess.check_call('mode.com con cols={} lines={}'.format(
+								cols, lines))
+		user32.ShowWindow(hWnd, SW_MAXIMIZE)
+maximize_console(9999)
 logging.basicConfig(level=logging.INFO)
 
 class Main_program:
@@ -212,3 +278,4 @@ Rocketman = Main_program()
 Rocketman.set_initial_conditions()
 Rocketman.initialize_rocket()
 Rocketman.sim_rocket()
+raw_input("Sim complete. Hit enter to exit")
